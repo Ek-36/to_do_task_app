ef5bb99e9cefb6a2d3adf5f70b08489e599d4736
Edwin <edwinmark336@gmail.com>
2026-02-16 09:13:20 +0530

    Initial commit

---
+++ src/test/java/com/edwin/task/TaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
